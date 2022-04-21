6f00e55807a66bc5ca7133fd6ecdde59b91b59f0
Ingrid <65837228+IngrdCS@users.noreply.github.com>
2022-04-20 23:59:13 -0300

    atualizando

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-
+// cadastro básico js
 alert('Sistema de cadastro de Usuário!');
 
 let nomeDousuário = prompt('Digite o nome completo do usuário:');
@@ -14,12 +14,12 @@ let bairro = prompt('Nome do bairro:');
 let cidade = prompt('Nome da cidade:');
 let estado = prompt('Nome do estado onde mora:');
 
-alert('Agora, já temos o endereço. Pressione OK');
+alert('Usuário cadastrado com sucesso! Pressione OK');
 
-alert('Obrigado! Agora pressione enter para conferir os dados')
+alert('Obrigado! Agora pressione enter para conferir os dados');
 
 alert(`Dados do paciente:\n Nome: ${nomeDousuário}\n Idade: ${idade}\n Sexo: ${sexo}\n Nome da rua: ${nomeDaRua}\n Número da rua: ${numRua}\n 
-Nome do bairro: ${bairro}\n Cidade: ${cidade}\n Estado: ${estado}\n Comorbidades: ${comorbidades} \n\n Usuário cadastrado com sucesso!`);
+Nome do bairro: ${bairro}\n Cidade: ${cidade}\n Estado: ${estado}\n Comorbidades: ${comorbidades} \n\n Até mais!`);
 
 
 
